7999d7e074f405e6b225dea91210755acdd98e74
jamietwells <jtwells3@googlemail.com>
2019-07-26 16:34:31 +0100

    Import project

---
+++ build/precache-manifest.2e987cf3710bcb67b3c2f09e54d8bcd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751edc8f89e704eb74d9e541f297b1a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b42f4e2210c7d348320b",
+    "url": "/static/css/main.9e57db48.chunk.css"
+  },
+  {
+    "revision": "4f7c07eb1cc81b8822b1",
+    "url": "/static/js/2.b7563092.chunk.js"
+  },
+  {
+    "revision": "b42f4e2210c7d348320b",
+    "url": "/static/js/main.6a437677.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4fb39f209c6a0b4fbb8510d3bddc29ec",
+    "url": "/static/media/github-icon-dark.4fb39f20.svg"
+  }
+]);
