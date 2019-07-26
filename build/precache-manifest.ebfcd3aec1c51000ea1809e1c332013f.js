dba49975a0c541288675e4b5ddddb87993c7d907
Jamie Twells <jtwells3@googlemail.com>
2019-07-26 19:17:57 +0100

    Adding new build folder

---
+++ build/precache-manifest.ebfcd3aec1c51000ea1809e1c332013f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231c1131c930895ea0eae2ce6c2967ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481ed4e7ffa1a354f8ab",
+    "url": "/static/css/main.8bf699ba.chunk.css"
+  },
+  {
+    "revision": "4f7c07eb1cc81b8822b1",
+    "url": "/static/js/2.b7563092.chunk.js"
+  },
+  {
+    "revision": "481ed4e7ffa1a354f8ab",
+    "url": "/static/js/main.35368ab2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9a25e933f269833d22e5d7843a68069e",
+    "url": "/static/media/github-icon-dark.9a25e933.svg"
+  }
+]);
